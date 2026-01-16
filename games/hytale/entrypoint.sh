194ed3d21d09fe71223d6c35c17715caa24df038
zzahkaboom24 <github@zzahkaboom24.de>
2026-01-16 02:28:25 +0100

    Update entrypoint.sh

--- games/hytale/entrypoint.sh
+++ games/hytale/entrypoint.sh
@@ -71,4 +71,36 @@ if [[ -f config.json && -n "$HYTALE_MAX_VIEW_RADIUS" ]]; then
 	jq ".MaxViewRadius = $HYTALE_MAX_VIEW_RADIUS" config.json > config.tmp.json && mv config.tmp.json config.json
 fi
 
+# Re-train the Ahead-of-Time cache, because the one provided by Hytale can't load due to an "timestamp has changed" error
+train_aot() {
+	if [ -f "./Server/HytaleServer.aot" ]; then
+		rm -f ./Server/HytaleServer.aot
+	fi
+
+	: > ./Server/training.log
+	
+	java -XX:AOTCacheOutput=Server/HytaleServer.aot -Xms128M $( ((SERVER_MEMORY)) && printf %s "-Xmx${SERVER_MEMORY}M" ) -jar Server/HytaleServer.jar $( ((HYTALE_ALLOW_OP)) && printf %s "--allow-op" ) $( ((HYTALE_ACCEPT_EARLY_PLUGINS)) && printf %s "--accept-early-plugins" ) $( ((DISABLE_SENTRY)) && printf %s "--disable-sentry" ) --auth-mode ${HYTALE_AUTH_MODE} --assets Assets.zip --bind 0.0.0.0:${SERVER_PORT} > ./Server/training.log 2>&1 &
+	PID=$!
+
+	tail -f ./Server/training.log | while read -r LINE; do
+		echo "${LINE}"
+
+		if [[ "${LINE}" == *"Hytale Server Booted"* ]]; then
+			echo -e "Detected 'Hytale Server Booted'..."
+			touch ./Server/aot-retrained.info
+			rm -f ./Server/training.log
+			echo -e "Training finished. AOT cache created: HytaleServer.aot. Restarting server!"
+			kill -TERM ${PID}
+			break
+		fi
+	done
+
+	wait ${PID}
+}
+
+if [ ! -f "./Server/aot-retrained.info" ]; then
+    train_aot
+fi
+
+
 /java.sh $@

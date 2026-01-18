b2e2ff6999c1ce66a0f2def27bbe3a3ecf0ea8ec
zzahkaboom24 <github@zzahkaboom24.de>
2026-01-18 21:22:37 +0100

    Update entrypoint.sh

--- games/hytale/entrypoint.sh
+++ games/hytale/entrypoint.sh
@@ -150,6 +150,11 @@ train_aot() {
 }
 
 if [[ "${USE_AOT_CACHE}" == "1" ]]; then
+	if (( SERVER_MEMORY > 32768 )); then
+		export JAVA_TOOL_OPTIONS="-XX:-UseCompressedOops -XX:-UseCompressedClassPointers"
+	else
+		export JAVA_TOOL_OPTIONS="-XX:+UseCompressedOops -XX:+UseCompressedClassPointers"
+	fi
 	if [[ "$NEEDS_DOWNLOAD" == true || ! -f config.json ]]; then
 		train_aot
 	elif [[ -f config.json && "$NEEDS_DOWNLOAD" == false ]]; then

b5fa2eecf5ff67a004a8a200a1a0dd7f99d9a60a
zzahkaboom24 <github@zzahkaboom24.de>
2026-01-22 02:10:09 +0100

    Remove game arguments from training

--- games/hytale/entrypoint.sh
+++ games/hytale/entrypoint.sh
@@ -149,7 +149,7 @@ train_aot() {
 		MAX_HEAP=$SERVER_MEMORY
 	fi
 	
-	java -XX:AOTCacheOutput=./Server/HytaleServer.aot -Xms128M -Xmx"${MAX_HEAP}"M -jar ./Server/HytaleServer.jar $( ((HYTALE_ALLOW_OP)) && printf %s "--allow-op" ) $( ((HYTALE_ACCEPT_EARLY_PLUGINS)) && printf %s "--accept-early-plugins" ) $( ((DISABLE_SENTRY)) && printf %s "--disable-sentry" ) --auth-mode "${HYTALE_AUTH_MODE}" --assets ./Assets.zip --bind "0.0.0.0:${SERVER_PORT}" 2>&1 | tee ./Server/training.log
+	java -XX:AOTCacheOutput=./Server/HytaleServer.aot -Xms128M -Xmx"${MAX_HEAP}"M -jar ./Server/HytaleServer.jar --auth-mode "${HYTALE_AUTH_MODE}" --assets ./Assets.zip --bind "0.0.0.0:${SERVER_PORT}" 2>&1 | tee ./Server/training.log
 
 	TIMEOUT=30
 	while [[ ! -f "./Server/HytaleServer.aot" ]] && (( TIMEOUT > 0 )); do

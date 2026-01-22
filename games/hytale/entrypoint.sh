bc3b049862bca7aeb97e38b05e6e0a07bb0f84f2
zzahkaboom24 <github@zzahkaboom24.de>
2026-01-22 14:45:03 +0100

    Testing out the official hytale pterodactyl egg and why it isnt booting

--- games/hytale/entrypoint.sh
+++ games/hytale/entrypoint.sh
@@ -1,81 +1,34 @@
 #!/bin/bash
 set -e
+set -x
 
 cd /home/container
 
-# Default to false; We don't assume people to be providing the files themselves
-HYTALE_MOUNT=false
-if [[ -f "./HytaleMount/HytaleServer.zip" || -f "./HytaleMount/Assets.zip" ]]; then
-	HYTALE_MOUNT=true
-fi
-
-# Default to downloading (unless we find matching version)
-NEEDS_DOWNLOAD=true
-
 # If HYTALE_SERVER_SESSION_TOKEN isn't set, assume the user will log in themselves, rather than a host's GSP
 if [[ -z "$HYTALE_SERVER_SESSION_TOKEN" ]]; then
-	if [[ "$(uname -m)" == "aarch64" ]]; then
-		HYTALE_DOWNLOADER="qemu-x86_64-static ./hytale-downloader/hytale-downloader-linux"
-	else
-		HYTALE_DOWNLOADER="./hytale-downloader/hytale-downloader-linux"
-	fi
-	
-	if [[ -f "./Server/HytaleServer.jar" ]]; then
-	LATEST_VERSION=$($HYTALE_DOWNLOADER -print-version)
-		if [[ -f config.json ]]; then
-			if [[ "$(jq -r '.ServerVersion // ""' config.json)" != "" ]]; then
-				CURRENT_VERSION=$(jq -r '.ServerVersion' config.json)
-			else
-				CURRENT_VERSION=$(java -jar ./Server/HytaleServer.jar --version | awk '{print $2}' | sed 's/^v//')
-			fi
-		else
-			CURRENT_VERSION=$(java -jar ./Server/HytaleServer.jar --version | awk '{print $2}' | sed 's/^v//')
-		fi
-		if [[ "$CURRENT_VERSION" != "$LATEST_VERSION" ]]; then
-			echo -e "Server is out-of-date!"
-			echo -e "Currently installed: $CURRENT_VERSION"
-			echo -e "Latest available: $LATEST_VERSION"
-			NEEDS_DOWNLOAD=true
-		else
-			echo -e "Server is up-to-date!"
-        	NEEDS_DOWNLOAD=false
-		fi
-	else
-		echo -e "Server has not yet been installed!"
-		echo -e "Attempting install!"
-	fi
+        
+	curversion=$(./hytale-downloader/hytale-downloader-linux -print-version)
+        
+	if ! [[ -e version ]] || [ "$curversion" != "$(cat "version")" ]; then
+		./hytale-downloader/hytale-downloader-linux -patchline "$HYTALE_PATCHLINE" -download-path HytaleServer.zip
 
-	if [[ "$NEEDS_DOWNLOAD" == true ]]; then
-		if [[ -f "./Server/HytaleServer.jar" ]]; then
-			rm -rf ./Server/*
-		fi
-		$HYTALE_DOWNLOADER -patchline "$HYTALE_PATCHLINE" -download-path HytaleServer.zip
-	fi
-
-	if [[ -f "HytaleServer.zip" ]]; then
 		unzip -o HytaleServer.zip -d .
 		rm -f HytaleServer.zip
+		echo "$curversion" > version
 	fi
-fi
-
-if [[ "$HYTALE_MOUNT" == true ]]; then
-	if [[ -f "HytaleMount/HytaleServer.zip" ]]; then
-		unzip -o HytaleMount/HytaleServer.zip -d .
-	fi
-	if [[ -f "HytaleMount/Assets.zip" ]]; then
-		ln -s -f HytaleMount/Assets.zip Assets.zip
-	fi
-else
-	if [[ -f "Server/Assets.zip" ]]; then
-		ln -s -f Server/Assets.zip Assets.zip
-	fi
-	if [[ -f "HytaleServer.zip" ]]; then
-		unzip -o HytaleServer.zip -d .
-	fi
+	
+elif [[ -f "HytaleMount/HytaleServer.zip" ]]; then
+	unzip -o HytaleMount/HytaleServer.zip -d .
+elif [[ -f "HytaleMount/Assets.zip" ]]; then
+	ln -s -f HytaleMount/Assets.zip Assets.zip
+elif [[ -f "Server/Assets.zip" ]]; then
+	ln -s -f Server/Assets.zip Assets.zip
+elif [[ -f "HytaleServer.zip" ]]; then
+	unzip -o HytaleServer.zip -d .
 fi
 
 # Download the latest hytale-sourcequery plugin if enabled
-if [[ "${INSTALL_SOURCEQUERY_PLUGIN}" == "1" ]]; then
+if [ "${INSTALL_SOURCEQUERY_PLUGIN}" == "1" ]; then
 	mkdir -p mods
 	echo -e "Downloading latest hytale-sourcequery plugin..."
 	LATEST_URL=$(curl -sSL https://api.github.com/repos/physgun-com/hytale-sourcequery/releases/latest \
@@ -88,118 +41,8 @@ if [[ "${INSTALL_SOURCEQUERY_PLUGIN}" == "1" ]]; then
 	fi
 fi
 
-# This section restores custom values in the config.json
-# Custom values are lost if an user runs /auth persistence Memory/Encrypted
-if [[ -f config.json && -f config.json.bak ]]; then
-	# Restore AheadOfTimeCacheTrained
-	if [[ -z "$(jq -r '.AheadOfTimeCacheTrained // ""' config.json)" ]]; then
-		if [[ ! -z "$(jq -r '.AheadOfTimeCacheTrained // ""' config.json.bak)" ]]; then
-			AOT_BACKUP_FLAG=$(jq -r '.AheadOfTimeCacheTrained' config.json.bak)
-			jq --argjson trainaot "$AOT_BACKUP_FLAG" '.AheadOfTimeCacheTrained = $trainaot' config.json > config.tmp.json && mv config.tmp.json config.json
-		fi
-	fi
-	# Restore ServerVersion
-	if [[ "$(jq -r '.ServerVersion // ""' config.json)" == "" ]]; then
-		if [[ "$(jq -r '.ServerVersion // ""' config.json.bak)" != "" ]]; then
-			SV_BACKUP_FLAG=$(jq -r '.ServerVersion // ""' config.json.bak)
-			jq --arg version "$SV_BACKUP_FLAG" '.ServerVersion = $version' config.json > config.tmp.json && mv config.tmp.json config.json
-		fi
-	fi
+if [[ -f config.json && -n "$HYTALE_MAX_VIEW_RADIUS" ]]; then
+	jq ".MaxViewRadius = $HYTALE_MAX_VIEW_RADIUS" config.json > config.tmp.json && mv config.tmp.json config.json
 fi
 
-MAX_HEAP=31744
-AOT_TRAINED=false
-# Re-train the Ahead-of-Time cache, because the one provided by Hytale can't load due to an "timestamp has changed" error
-train_aot() {
-	if [[ -f "./Server/HytaleServer.aot" ]]; then
-		rm -f ./Server/HytaleServer.aot
-	fi
-	if [[ -f "./Server/HytaleServer.aot.conf" ]]; then
-		rm -f ./Server/HytaleServer.aot.conf
-	fi
-	if [[ -f "./Server/training.log" ]]; then
-		rm -f ./Server/training.log
-	fi
-
-	touch ./Server/training.log
-
-	(
-		tail -f ./Server/training.log | while read -r LINE; do
-			echo "$LINE"
-			if [[ "$LINE" == *"Hytale Server Booted"* ]]; then
-				echo -e "Detected 'Hytale Server Booted'..."
-				break
-			fi
-		done
-
-		PID=$(pgrep -f "./Server/HytaleServer.jar")
-		echo -e "Triggering shutdown to generate AOT cache..."
-		kill -TERM "$PID"
-		echo -e "Training finished. Waiting for creation of AOT cache file..."
-	) &
-
-	if (( SERVER_MEMORY > MAX_HEAP )); then
-		MAX_HEAP=31744
-	elif (( SERVER_MEMORY == 0 )); then
-		MAX_HEAP=$(free -m | awk '/Mem:/ {print $2}')
-		if (( MAX_HEAP > 31744 )); then
-			MAX_HEAP=31744
-		fi
-	else
-		MAX_HEAP=$SERVER_MEMORY
-	fi
-	
-	java -XX:AOTCacheOutput=./Server/HytaleServer.aot -Xms128M -Xmx"${MAX_HEAP}"M -jar ./Server/HytaleServer.jar --auth-mode "${HYTALE_AUTH_MODE}" --assets ./Assets.zip --bind "0.0.0.0:${SERVER_PORT}" 2>&1 | tee ./Server/training.log
-
-	TIMEOUT=30
-	while [[ ! -f "./Server/HytaleServer.aot" ]] && (( TIMEOUT > 0 )); do
-		sleep 1
-		(( TIMEOUT-- ))
-	done
-	if [[ ! -f "./Server/HytaleServer.aot" ]]; then
-		echo -e "AOT file not found after 30s."
-	else
-		echo -e "AOT cache created: HytaleServer.aot. Restarting server..."
-	fi
-}
-
-if [[ "${USE_AOT_CACHE}" == "1" ]]; then
-	if (( SERVER_MEMORY > 31744 )); then
-		export JAVA_TOOL_OPTIONS="-XX:-UseCompressedOops -XX:-UseCompressedClassPointers"
-	elif (( SERVER_MEMORY == 0 )); then
-		MAX_HEAP=$(free -m | awk '/Mem:/ {print $2}')
-		if (( MAX_HEAP > 31744 )); then
-			export JAVA_TOOL_OPTIONS="-XX:-UseCompressedOops -XX:-UseCompressedClassPointers"
-		else
-			export JAVA_TOOL_OPTIONS="-XX:+UseCompressedOops -XX:+UseCompressedClassPointers"
-		fi
-	else
-		export JAVA_TOOL_OPTIONS="-XX:+UseCompressedOops -XX:+UseCompressedClassPointers"
-	fi
-	if [[ ! -f config.json || ! -f ./Server/HytaleServer.aot || "$NEEDS_DOWNLOAD" == true ]]; then
-		train_aot
-	elif [[ -f config.json && "$NEEDS_DOWNLOAD" == false ]]; then
-		if [[ "$(jq -r '.AheadOfTimeCacheTrained // ""' config.json)" != "true" ]]; then
-			train_aot
-		fi
-	fi
-else
-	AOT_TRAINED=false
-	jq --argjson trainaot "$AOT_TRAINED" '.AheadOfTimeCacheTrained = $trainaot' config.json > config.tmp.json && mv config.tmp.json config.json
-fi
-
-if [[ -f ./Server/training.log && -f config.json ]]; then
-		AOT_TRAINED=true
-		jq --argjson trainaot "$AOT_TRAINED" '.AheadOfTimeCacheTrained = $trainaot' config.json > config.tmp.json && mv config.tmp.json config.json
-		rm -f ./Server/training.log
-fi
-
-if [[ -f config.json ]]; then
-	if [[ -n "$HYTALE_MAX_VIEW_RADIUS" ]]; then
-		jq --argjson maxviewradius "$HYTALE_MAX_VIEW_RADIUS" '.MaxViewRadius = $maxviewradius' config.json > config.tmp.json && mv config.tmp.json config.json
-	fi
-	LATEST_VERSION=$($HYTALE_DOWNLOADER -print-version)
-	jq --arg version "$LATEST_VERSION" '.ServerVersion = $version' config.json > config.tmp.json && mv config.tmp.json config.json
-fi
-	
 /java.sh $@

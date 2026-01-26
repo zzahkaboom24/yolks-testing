0441643ef1ff3923a08fb47b72a8220ae8539232
zzahkaboom24 <github@zzahkaboom24.de>
2026-01-26 22:43:38 +0100

    Update entrypoint.sh

--- games/hytale/entrypoint.sh
+++ games/hytale/entrypoint.sh
@@ -3,7 +3,6 @@ set -e
 
 cd /home/container
 
-# Migration from old path to new path
 if [[ -f ./config.json || -f ./HytaleServer.jar || -f ./HytaleServer.aot || -f ./whitelist.json || -f ./bans.json || -f ./whitelist.json ]]; then
 	if [[ ! -d "/home/container/Server" ]]; then
 		mkdir -p /home/container/Server
@@ -32,6 +31,15 @@ if [[ -f ./Server/config.json ]]; then
 	fi
 fi
 
+# Default to false; We don't assume people to be providing the files themselves
+HYTALE_MOUNT=false
+if [[ -f "./HytaleMount/HytaleServer.zip" || -f "./HytaleMount/Assets.zip" ]]; then
+	HYTALE_MOUNT=true
+fi
+
+# Default to downloading (unless we find matching version)
+NEEDS_DOWNLOAD=true
+
 # If HYTALE_SERVER_SESSION_TOKEN isn't set, assume the user will log in themselves, rather than a host's GSP
 if [[ -z "$HYTALE_SERVER_SESSION_TOKEN" ]]; then
 	if [[ "$(uname -m)" == "aarch64" ]]; then
@@ -40,10 +48,12 @@ if [[ -z "$HYTALE_SERVER_SESSION_TOKEN" ]]; then
 		HYTALE_DOWNLOADER="./hytale-downloader/hytale-downloader-linux"
 	fi
 
+	"$HYTALE_DOWNLOADER" -patchline "$HYTALE_PATCHLINE" -print-version
+	LATEST_VERSION=$("$HYTALE_DOWNLOADER" -patchline "$HYTALE_PATCHLINE" -print-version)
+
 	# Apply staged update if present
 	if [[ -f "./updater/staging/Server/HytaleServer.jar" ]]; then
-		curversion=$($HYTALE_DOWNLOADER -patchline "$HYTALE_PATCHLINE" -print-version | tee /dev/tty)
-		echo "[Launcher] Applying $curversion update..."
+		echo "[Launcher] Applying $LATEST_VERSION update..."
 		# Only replace update files, preserve config.json/universe/mods
 		cp -f ./updater/staging/Server/HytaleServer.jar ./Server/
 		if [[ -f "./updater/staging/Server/HytaleServer.aot" ]]; then
@@ -64,40 +74,65 @@ if [[ -z "$HYTALE_SERVER_SESSION_TOKEN" ]]; then
 		#fi
 
 		rm -rf ./updater/staging
-		echo "$curversion" > ./version
-	fi
-	
-	echo -e "Checking for Hytale server update..."
-
-	if [[ -f ./version ]]; then
-		curversion=$("$HYTALE_DOWNLOADER" -patchline "$HYTALE_PATCHLINE" -print-version | tee /dev/tty)
-	fi
-
-	if ! [[ -e ./version ]] || [ "$curversion" != "$(cat "./version")" ]; then
-		if [[ -f ./version ]]; then
-			echo -e "New update available, downloading version $curversion..."
+		if [[ -f ./Server/config.json ]]; then
+			jq --arg version "$LATEST_VERSION" '.ServerVersion = $version' ./Server/config.json > ./Server/config.tmp.json && mv ./Server/config.tmp.json ./Server/config.json
 		fi
-
-		"$HYTALE_DOWNLOADER" -patchline "$HYTALE_PATCHLINE" -download-path HytaleServer.zip
-
-		# Write the current version if it wasn't set before
-		if [[ -z "$curversion" ]]; then
-			curversion=$("$HYTALE_DOWNLOADER" -patchline "$HYTALE_PATCHLINE" -print-version | tee /dev/tty)
+	elif [[ -f "./Server/HytaleServer.jar" ]]; then
+		if [[ -f ./Server/config.json ]]; then
+			if [[ "$(jq -r '.ServerVersion // ""' ./Server/config.json)" != "" ]]; then
+				CURRENT_VERSION=$(jq -r '.ServerVersion' ./Server/config.json)
+			else
+				CURRENT_VERSION=$(java -jar ./Server/HytaleServer.jar --version | awk '{print $2}' | sed 's/^v//')
+			fi
+		else
+			CURRENT_VERSION=$(java -jar ./Server/HytaleServer.jar --version | awk '{print $2}' | sed 's/^v//')
 		fi
+		if [[ "$CURRENT_VERSION" != "$LATEST_VERSION" ]]; then
+			echo -e "Server is out-of-date!"
+			echo -e "Currently installed: $CURRENT_VERSION"
+			echo -e "Latest available: $LATEST_VERSION"
+			NEEDS_DOWNLOAD=true
+		else
+			echo -e "Server is up-to-date!"
+			echo -e "Currently installed: $CURRENT_VERSION"
+			echo -e "Latest available: $LATEST_VERSION"
+        	NEEDS_DOWNLOAD=false
+		fi
+	else
+		echo -e "Server has not yet been installed!"
+		echo -e "Attempting install!"
+	fi
 
+	if [[ "$NEEDS_DOWNLOAD" == true ]]; then
+		if [[ -f "./Server/HytaleServer.jar" ]]; then
+			rm -rf ./Assets.zip
+			rm -rf ./Server/HytaleServer.jar
+			rm -rf ./Server/HytaleServer.aot
+			rm -rf ./Server/Licenses
+		fi
+		"$HYTALE_DOWNLOADER" -patchline "$HYTALE_PATCHLINE" -download-path ./HytaleServer.zip
+	fi
+
+	if [[ -f "./HytaleServer.zip" ]]; then
 		unzip -o ./HytaleServer.zip -d .
 		rm -f ./HytaleServer.zip
-		echo "$curversion" > ./version
 	fi
+fi
 
-elif [[ -f "HytaleMount/HytaleServer.zip" ]]; then
-	unzip -o HytaleMount/HytaleServer.zip -d .
-elif [[ -f "HytaleMount/Assets.zip" ]]; then
-	ln -s -f HytaleMount/Assets.zip Assets.zip
-elif [[ -f "Server/Assets.zip" ]]; then
-	ln -s -f Server/Assets.zip Assets.zip
-elif [[ -f "HytaleServer.zip" ]]; then
-	unzip -o HytaleServer.zip -d .
+if [[ "$HYTALE_MOUNT" == true ]]; then
+	if [[ -f "./HytaleMount/HytaleServer.zip" ]]; then
+		unzip -o ./HytaleMount/HytaleServer.zip -d .
+	fi
+	if [[ -f "./HytaleMount/Assets.zip" ]]; then
+		ln -s -f ./HytaleMount/Assets.zip Assets.zip
+	fi
+else
+	if [[ -f "./Server/Assets.zip" ]]; then
+		ln -s -f ./Server/Assets.zip Assets.zip
+	fi
+	if [[ -f "./HytaleServer.zip" ]]; then
+		unzip -o ./HytaleServer.zip -d .
+	fi
 fi
 
 # Removing launch scripts, because I don't believe them to be necessary.
@@ -113,7 +148,7 @@ if [[ -f start.sh ]]; then
 fi
 
 # Download the latest hytale-sourcequery plugin if enabled
-if [ "${INSTALL_SOURCEQUERY_PLUGIN}" == "1" ]; then
+if [[ "${INSTALL_SOURCEQUERY_PLUGIN}" == "1" ]]; then
 	mkdir -p ./Server/mods
 	echo -e "Downloading latest hytale-sourcequery plugin..."
 	LATEST_URL=$(curl -sSL https://api.github.com/repos/physgun-com/hytale-sourcequery/releases/latest \
@@ -126,8 +161,118 @@ if [ "${INSTALL_SOURCEQUERY_PLUGIN}" == "1" ]; then
 	fi
 fi
 
-if [[ -f ./Server/config.json && -n "$HYTALE_MAX_VIEW_RADIUS" ]]; then
-	jq ".MaxViewRadius = $HYTALE_MAX_VIEW_RADIUS" ./Server/config.json > ./Server/config.tmp.json && mv ./Server/config.tmp.json ./Server/config.json
+# This section restores custom values in the config.json
+# Custom values are lost if an user runs /auth persistence Memory/Encrypted
+if [[ -f ./Server/config.json && -f ./Server/config.json.bak ]]; then
+	# Restore AheadOfTimeCacheTrained
+	if [[ -z "$(jq -r '.AheadOfTimeCacheTrained // ""' ./Server/config.json)" ]]; then
+		if [[ ! -z "$(jq -r '.AheadOfTimeCacheTrained // ""' ./Server/config.json.bak)" ]]; then
+			AOT_BACKUP_FLAG=$(jq -r '.AheadOfTimeCacheTrained' ./Server/config.json.bak)
+			jq --argjson trainaot "$AOT_BACKUP_FLAG" '.AheadOfTimeCacheTrained = $trainaot' ./Server/config.json > ./Server/config.tmp.json && mv ./Server/config.tmp.json ./Server/config.json
+		fi
+	fi
+	# Restore ServerVersion
+	if [[ "$(jq -r '.ServerVersion // ""' ./Server/config.json)" == "" ]]; then
+		if [[ "$(jq -r '.ServerVersion // ""' ./Server/config.json.bak)" != "" ]]; then
+			SV_BACKUP_FLAG=$(jq -r '.ServerVersion // ""' ./Server/config.json.bak)
+			jq --arg version "$SV_BACKUP_FLAG" '.ServerVersion = $version' ./Server/config.json > ./Server/config.tmp.json && mv ./Server/config.tmp.json ./Server/config.json
+		fi
+	fi
+fi
+
+MAX_HEAP=31744
+AOT_TRAINED=false
+# Re-train the Ahead-of-Time cache, because the one provided by Hytale can't load due to an "timestamp has changed" error
+train_aot() {
+	if [[ -f "./Server/HytaleServer.aot" ]]; then
+		rm -f ./Server/HytaleServer.aot
+	fi
+	if [[ -f "./Server/HytaleServer.aot.conf" ]]; then
+		rm -f ./Server/HytaleServer.aot.conf
+	fi
+	if [[ -f "./Server/training.log" ]]; then
+		rm -f ./Server/training.log
+	fi
+
+	touch ./Server/training.log
+
+	(
+		tail -f ./Server/training.log | while read -r LINE; do
+			echo "$LINE"
+			if [[ "$LINE" == *"Hytale Server Booted"* ]]; then
+				echo -e "Detected 'Hytale Server Booted'..."
+				break
+			fi
+		done
+
+		PID=$(pgrep -f "./Server/HytaleServer.jar")
+		echo -e "Triggering shutdown to generate AOT cache..."
+		kill -TERM "$PID"
+		echo -e "Training finished. Waiting for creation of AOT cache file..."
+	) &
+
+	if (( SERVER_MEMORY > MAX_HEAP )); then
+		MAX_HEAP=31744
+	elif (( SERVER_MEMORY == 0 )); then
+		MAX_HEAP=$(free -m | awk '/Mem:/ {print $2}')
+		if (( MAX_HEAP > 31744 )); then
+			MAX_HEAP=31744
+		fi
+	else
+		MAX_HEAP=$SERVER_MEMORY
+	fi
+	
+	java -XX:AOTCacheOutput=./Server/HytaleServer.aot -Xms128M -Xmx"${MAX_HEAP}"M -jar ./Server/HytaleServer.jar --auth-mode "${HYTALE_AUTH_MODE}" --assets ./Assets.zip --bind "0.0.0.0:${SERVER_PORT}" 2>&1 | tee ./Server/training.log
+
+	TIMEOUT=30
+	while [[ ! -f "./Server/HytaleServer.aot" ]] && (( TIMEOUT > 0 )); do
+		sleep 1
+		(( TIMEOUT-- ))
+	done
+	if [[ ! -f "./Server/HytaleServer.aot" ]]; then
+		echo -e "AOT file not found after 30s."
+	else
+		echo -e "AOT cache created: HytaleServer.aot. Restarting server..."
+	fi
+}
+
+if [[ "${USE_AOT_CACHE}" == "1" ]]; then
+	if (( SERVER_MEMORY > 31744 )); then
+		export JAVA_TOOL_OPTIONS="-XX:-UseCompressedOops -XX:-UseCompressedClassPointers"
+	elif (( SERVER_MEMORY == 0 )); then
+		MAX_HEAP=$(free -m | awk '/Mem:/ {print $2}')
+		if (( MAX_HEAP > 31744 )); then
+			export JAVA_TOOL_OPTIONS="-XX:-UseCompressedOops -XX:-UseCompressedClassPointers"
+		else
+			export JAVA_TOOL_OPTIONS="-XX:+UseCompressedOops -XX:+UseCompressedClassPointers"
+		fi
+	else
+		export JAVA_TOOL_OPTIONS="-XX:+UseCompressedOops -XX:+UseCompressedClassPointers"
+	fi
+	if [[ ! -f ./Server/config.json || ! -f ./Server/HytaleServer.aot || "$NEEDS_DOWNLOAD" == true ]]; then
+		train_aot
+	elif [[ -f ./Server/config.json && "$NEEDS_DOWNLOAD" == false ]]; then
+		if [[ "$(jq -r '.AheadOfTimeCacheTrained // ""' ./Server/config.json)" != "true" ]]; then
+			train_aot
+		fi
+	fi
+else
+	AOT_TRAINED=false
+	jq --argjson trainaot "$AOT_TRAINED" '.AheadOfTimeCacheTrained = $trainaot' ./Server/config.json > ./Server/config.tmp.json && mv ./Server/config.tmp.json ./Server/config.json
+fi
+
+if [[ -f ./Server/training.log && -f ./Server/config.json ]]; then
+		AOT_TRAINED=true
+		jq --argjson trainaot "$AOT_TRAINED" '.AheadOfTimeCacheTrained = $trainaot' ./Server/config.json > ./Server/config.tmp.json && mv ./Server/config.tmp.json ./Server/config.json
+		rm -f ./Server/training.log
+fi
+
+if [[ -f ./Server/config.json ]]; then
+	if [[ -n "$HYTALE_MAX_VIEW_RADIUS" ]]; then
+		jq --argjson maxviewradius "$HYTALE_MAX_VIEW_RADIUS" '.MaxViewRadius = $maxviewradius' ./Server/config.json > ./Server/config.tmp.json && mv ./Server/config.tmp.json ./Server/config.json
+	fi
+	LATEST_VERSION=$("$HYTALE_DOWNLOADER" -patchline "$HYTALE_PATCHLINE" -print-version)
+	jq --arg version "$LATEST_VERSION" '.ServerVersion = $version' ./Server/config.json > ./Server/config.tmp.json && mv ./Server/config.tmp.json ./Server/config.json
 fi
 
 if [[ ! -d "/home/container/Server" ]]; then

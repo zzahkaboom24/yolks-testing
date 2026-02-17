3f737bce6b4653ad4da6f4d332960474dce6b3fd
zzahkaboom24 <github@zzahkaboom24.de>
2026-02-17 19:55:45 +0100

    Update entrypoint.sh

--- games/hytale/entrypoint.sh
+++ games/hytale/entrypoint.sh
@@ -14,10 +14,13 @@ if [[ -z "$HYTALE_SERVER_SESSION_TOKEN" ]]; then
 
 	if ! [[ -e version ]] || [ "$curversion" != "$(cat "version")" ]; then
 		if [[ -f version ]]; then
-			echo -e "New update available, downloading version $curversion..."
+			if [[ "${SKIP_UPDATE}" == "1" ]]; then
+				echo -e "New update available: $curversion"
+			else
+				echo -e "New update available, downloading version $curversion..."
+				./hytale-downloader/hytale-downloader-linux -patchline "$HYTALE_PATCHLINE" -download-path HytaleServer.zip
+			fi
 		fi
-
-		./hytale-downloader/hytale-downloader-linux -patchline "$HYTALE_PATCHLINE" -download-path HytaleServer.zip
 
 		# Write the current version if it wasn't set before
 		if [[ -z "$curversion" ]]; then
